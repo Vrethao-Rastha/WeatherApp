946ec7f56a54b5b5c4c7f6ed7789123105593f0f
BryanCox <bryanc71@yahoo.com>
2018-04-03 13:07:58 -0700

    asdf

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import WeatherMain from './WeatherMain';
 
 class App extends Component {
   render() {
     return (
-      
+      <WeatherMain />
     );
   }
 }
